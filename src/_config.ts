13c9d4313cd82a1bc9f6752db94ab40afcdd474c
X <1606931416@qq.com>
2023-12-21 18:38:03 +0800

    1

--- src/_config.ts
+++ src/_config.ts
@@ -203,7 +203,7 @@ export const db_local_enabled: io = 1;
 export const local_cache_secret = process.env.local_cache_secret || "";
 //PostgreSQL配置(需Read/Write权限)
 import { Pool } from "@neondatabase/serverless"; //导入(不用改)
-export const db_bitio_enabled: io = 0; //启用postgresql数据库
+export const db_bitio_enabled: io = 1; //启用postgresql数据库
 const connectionString = //三种配置方法
   process.env.POSTGRES_URL || //在Vercel项目Storage里连接数据库，并将上方 db_bitio_enabled 设为1
   process.env.db_bitio_pool || //配置环境变量 db_bitio_pool 为下方格式
@@ -221,9 +221,9 @@ export const db_NOTION_log = process.env.NOTION_db_log || "";
 
 //======================黑白名单设置============================
 //黑名单模式 1-开 0-关
-export const blacklist_enabled: io = 1;
+export const blacklist_enabled: io = 0;
 //白名单模式 1-开 0-关
-export const whitelist_enabled: io = 0;
+export const whitelist_enabled: io = 1;
 //验证vip与视频要求 1-开 0-关
 export const check_vip_enabled: io = 1;
 //禁忌设置-白名单调用vip access_key 1-开 0-关
@@ -241,7 +241,7 @@ export const public_blacklist: string = "https://black.qimo.ink/api/users/";
 //要求登录 1-开 0-关
 export const need_login: io = 1;
 //允许WEB版使用(B站官网可直接请求,无需开启此选项) 1-开 0-关
-export const web_on: io = 0;
+export const web_on: io = 1;
 //允许Referer为 https://www.bilibili.com 的请求而无需打开web_on (解决BBDown问题) 1-开 0-关
 //用BBDown的打开此选项(虽然似乎没用)
 export const pass_web_on_check: io = 1;
